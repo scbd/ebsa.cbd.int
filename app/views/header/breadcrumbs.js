3abbf4de4127501b1074e926cd709ed5a30d8d87
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2014-03-06 23:33:30 -0500

    make breadcrumbs disappear on index page

--- app/views/header/breadcrumbs.js
+++ app/views/header/breadcrumbs.js
@@ -1,8 +1,18 @@
 define(['app'], function(app) {
 
-    return app.controller('BreadcrumbsCtrl', ['$scope', 'breadcrumbs', function ($scope, breadcrumbs) {
-      breadcrumbs.init();
-      $scope.breadcrumbs = breadcrumbs;
-      $scope.hasCrumbs = breadcrumbs.getAll().length;
-    }]);
+  return app.controller('BreadcrumbsCtrl', ['$rootScope', '$scope', '$location', 'breadcrumbs',
+    function($rootScope, $scope, $location, breadcrumbs) {
+      // breadcrumbs.init();
+
+      function computeCrumbs () {
+        $scope.breadcrumbs = breadcrumbs;
+        $scope.hasCrumbs = breadcrumbs.getAll().length;
+        $scope.showCrumbs = $location.path() !== '/';
+      }
+
+      $rootScope.$on('$routeChangeSuccess', function(e, route) {
+        computeCrumbs();
+      });
+    }
+  ]);
 });
