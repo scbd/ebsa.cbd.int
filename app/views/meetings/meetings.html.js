b6a176d278e9dad150bc87c9c185a550709c9b34
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2014-03-11 11:31:51 -0400

    pushing to force a change

--- app/views/meetings/meetings.html.js
+++ app/views/meetings/meetings.html.js
@@ -45,7 +45,6 @@ define(['app', '../../services/meetingService.js', 'underscore'], function(app, 
       $scope.dateInfo = {
         year: date.getFullYear()
       };
-      console.log($scope.dateInfo);
     }
   ]);
 
