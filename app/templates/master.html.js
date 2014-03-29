81f5602642f8968e5371b6d7bb84e058a906f253
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2014-03-28 22:26:11 -0400

    fix scrolling page back to top on route change

--- app/templates/master.html.js
+++ app/templates/master.html.js
@@ -1,10 +1,12 @@
 define(['app'], function(app) {
   'use strict';
 
-  app.controller('MasterTemplateController', ['$scope',
-    function($scope, breadcrumbs) {
-
-      $scope.controller = 'MasterTemplateController';
+  app.controller('MasterTemplateController', ['$scope', '$rootScope', '$anchorScroll',
+    function($scope, $rootScope, $anchorScroll) {
+      $rootScope.$on('$routeChangeSuccess', function(event) {
+        // snap the page back to the top on route change.
+        $anchorScroll();
+      });
     }
   ]);
 
