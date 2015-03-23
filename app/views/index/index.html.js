c7323e1d904aeb97030839c7fe6f192ad5578de6
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-03-23 11:52:48 -0400

    Show loading gear

--- app/views/index/index.html.js
+++ app/views/index/index.html.js
@@ -3,32 +3,39 @@ define(['app'], function(app) {
 
   return app.controller('IndexCtrl', ['$scope', 'geojson', 'meetings',
     function($scope, regions, Meetings) {
-      var regionData = {},
-        regionList = ['caribbean', 'southPacific'];
+      var regionData = {};
+      var regionList = ['caribbean', 'southPacific'];
 
       $scope.showFeature = function(regionName) {
         $scope.selectedRegion = regionName;
       };
 
-      regionList.forEach(function(regionName, index) {
+      $scope.loading = 2 + regionList.length;
+
+      regionList.forEach(function(regionName) {
         regions.getRegionByName(regionName, function(regionGeojson) {
           regionData[regionName] = regionGeojson;
           // check to see if we loaded everything and update the $scope.
           if (Object.keys(regionData).length === regionList.length) {
             $scope.regionData = regionData;
           }
+          $scope.loading --;
         });
       });
 
       Meetings.getMeetingsPage({timeframe: 'upcoming'})
         .then(function(meetingSet) {
           $scope.meetingsUpcoming = meetingSet;
+          $scope.loading --;
         });
 
       Meetings.getMeetingsPage({timeframe: 'previous'})
         .then(function(meetingSet) {
           $scope.meetingsPrevious = meetingSet;
+          $scope.loading --;
         });
+
+
     }
   ]);
 });
