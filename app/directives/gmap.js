b4eb9958f649f7a4bf7c3e595c438822469b7fcb
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-06-11 15:22:46 -0400

    fix http-https bug

--- app/directives/gmap.js
+++ app/directives/gmap.js
@@ -102,7 +102,7 @@ define(['./module.js', '../util/colors.js', 'underscore'],
             selectedRegion: '='
           },
           link: function(scope, element, attrs) {
-            require(['async!http://maps.google.com/maps/api/js?v=3.exp&sensor=false'], function(maps) {
+            require(['async!https://maps.google.com/maps/api/js?v=3.exp&sensor=false'], function(maps) {
               init(element.get(0));
 
 
