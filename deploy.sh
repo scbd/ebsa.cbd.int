b1e0c4cf20240f8877676eeec303fc9827972975
Philippe Leblond <philippe.leblond@cbd.int>
2016-01-07 15:06:59 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker build -t localhost:5000/ebsa-portal git@github.com:scbd/ebsa.cbd.int
+docker build -t localhost:5000/ebsa-portal git@github.com:scbd/ebsa-portal
 docker push     localhost:5000/ebsa-portal
