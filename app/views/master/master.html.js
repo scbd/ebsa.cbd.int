8b9841d15411eec495bae23595feddf9fafda93c
Stephane <bilodeaustephane@gmail.com>
2020-05-29 13:46:01 -0400

    Fix error for ga on routeChange

--- app/views/master/master.html.js
+++ app/views/master/master.html.js
@@ -1,15 +1,19 @@
-define(['app'], function(app) {
+define(['app', 'jquery'], function(app, $) {
   'use strict';
 
-  app.controller('MasterTemplateController', ['$scope', '$rootScope', '$anchorScroll',
-    function($scope, $rootScope, $anchorScroll) {
+  app.controller('MasterTemplateController', ['$scope', '$rootScope', '$anchorScroll', '$window', '$location',
+    function($scope, $rootScope, $anchorScroll, $window, $location) {
       $rootScope.$on('$routeChangeSuccess', function(event) {
         // snap the page back to the top on route change.
         $anchorScroll();
         $rootScope.$on('$routeChangeSuccess', function(){
-            $window.ga('set',  'page', basePath+$location.path());
+            $window.ga('set',  'page', basePath() + $location.path());
             $window.ga('send', 'pageview');
         });
+
+        function basePath() {
+          return $('head>base').attr('href').replace(/\/$/, '')
+        }
       });
     }
   ]);
