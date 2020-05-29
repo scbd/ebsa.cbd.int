f08c5c1bb316861349bafa6cbc9b0cdda49ac152
Stephane <bilodeaustephane@gmail.com>
2020-05-29 14:44:41 -0400

    Use CDN +  cleanup

--- app/boot.js
+++ app/boot.js
@@ -1,23 +1,27 @@
 /*jshint node:true*/
 
 'use strict';
-window.name = 'NG_DEFER_BOOTSTRAP!';
+var cdnHost = 'https://cdn.cbd.int/';
 
 require.config({
     baseUrl : './',
     waitSeconds : 120,
     paths: {
-        'angular'         : 'libs/angular/angular.min',
-        'angular-route'   : 'libs/angular-route/angular-route.min',
-        'ng-breadcrumbs'  : 'libs/es-breadcrumbs/dist/ng-breadcrumbs.min',
-        'async'           : 'libs/requirejs-plugins/src/async',
-        'domReady'        : 'libs/requirejs-domready/domReady',
-        'jquery'          : 'libs/jquery/jquery.min',
-        'bootstrap'       : 'libs/bootstrap/dist/js/bootstrap.min',
-        'underscore'      : 'libs/underscore/underscore-min',
+        'angular'         : cdnHost + 'angular@1.2.28/angular.min',
+        'angular-route'   : cdnHost + 'angular-route@1.2.28/angular-route.min',
+        'async'           : cdnHost + 'requirejs-plugins@1.0.2/src/async',
+        'jquery'          : cdnHost + 'jquery@2.2.4/dist/jquery.min',
+        'bootstrap'       : cdnHost + 'bootstrap@3.3.7/dist/js/bootstrap.min',
+        'underscore'      : cdnHost + 'underscore@1.10.2/underscore-min',
         'angular-growl'   : 'libs/angular-growl/build/angular-growl.min',
         'geojson-area'    : 'libs/geojson-area/index',
-        'gmapsapi'         : '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false'
+        'ng-breadcrumbs'  : 'libs/es-breadcrumbs/dist/ng-breadcrumbs.min',
+        'gmapsapi'         : '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false',
+
+        /*
+        'angular-growl'   : cdnHost + 'angular-growl@0.1.0/build/angular-growl',
+        
+        */
     },
     shim: {
         'angular'        : { 'deps': ['jquery'], 'exports': 'angular' },
@@ -29,13 +33,6 @@ require.config({
     }
 });
 
-require(['angular', 'domReady'],
- function (ng) {
-
-    // NOTE: place operations that need to initialize prior to app start here using the `run` function on the top-level module
-
-    require(['domReady!', './main'], function (document) {
-        ng.bootstrap(document, ['app']);
-        ng.resumeBootstrap();
-    });
+require(['angular', 'app', './main'], function(angular, app) {
+    angular.bootstrap(document, [app.name]);  
 });
--- test/unit.js
+++
@@ -1,6 +0,0 @@
-define([
-    '/base/test/unit/controllers/meetingsCtrl.spec.js',
-    '/base/test/unit/services/paginator.spec.js',
-  ], function() {
-    console.log('all tests loaded!');
-});
